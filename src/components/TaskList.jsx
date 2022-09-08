a1f095df2fadd17d4bccc55e50f6632de3ca8cae
Antoni <antoni.protoni@gmail.com>
2022-09-08 19:52:05 -0400

    setTaskList

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,60 +1,61 @@
 import { CheckCircle, Trash } from 'phosphor-react';
 import styles from './TaskList.module.css';
 export function TaskList() {
+  const tasks = [
+    {
+      id: 1,
+      title: 'Comprar HD',
+      isFinished: false,
+    },
+    {
+      id: 2,
+      title: 'Limpiar mesa de trabajo',
+      isFinished: false,
+    },
+    {
+      id: 3,
+      title: 'Hacer respaldo',
+      isFinished: false,
+    },
+    {
+      id: 4,
+      title: 'Llamar por telefono',
+      isFinished: false,
+    },
+    {
+      id: 5,
+      title: 'Revisar trabajo',
+      isFinished: false,
+    },
+    {
+      id: 6,
+      title: 'Formatear disco duro',
+      isFinished: true,
+    },
+  ];
+
   return (
     <>
       <div className={styles.tasklistContainer}>
         <header className={styles.taskListHeader}>
           <div>
             <strong className={styles.created}>Tareas creadas</strong>
-            <span>5</span>
+            <span>{tasks.length}</span>
           </div>
           <div>
             <strong className={styles.finished}>Concluidas</strong>
-            <span>2 de 5</span>
+            <span>2 de {tasks.length}</span>
           </div>
         </header>
         <ul className={styles.taskList}>
-          <li>
-            <CheckCircle size={20} weight='fill' color='var(--purple)' />
-            <p>
-              Fix the bugs Lorem ipsus af DDdasava advaV wf adipisicing F q
-              dolor siddt amet consectetur adipisicing elit. Impedit, asci?
-            </p>{' '}
-            <Trash size={20} />{' '}
-          </li>
-          <li>
-            <CheckCircle size={20} weight='fill' color='var(--purple)' />
-            <p>
-              Fix the bugs Lorem ipsus af DDdasava advaV wf adipisicing F q
-              dolor siddt amet consectetur adipisicing elit. Impedit, asci?
-            </p>{' '}
-            <Trash size={20} />{' '}
-          </li>
-          <li>
-            <CheckCircle size={20} weight='fill' color='var(--purple)' />
-            <p>
-              Fix the bugs Lorem ipsus af DDdasava advaV wf adipisicing F q
-              dolor siddt amet consectetur adipisicing elit. Impedit, asci?
-            </p>{' '}
-            <Trash size={20} />{' '}
-          </li>
-          <li>
-            <CheckCircle size={20} weight='fill' color='var(--purple)' />
-            <p>
-              Fix the bugs Lorem ipsus af DDdasava advaV wf adipisicing F q
-              dolor siddt amet consectetur adipisicing elit. Impedit, asci?
-            </p>{' '}
-            <Trash size={20} />{' '}
-          </li>
-          <li>
-            <CheckCircle size={20} weight='fill' color='var(--purple)' />
-            <p>
-              Fix the bugs Lorem ipsus af DDdasava advaV wf adipisicing F q
-              dolor siddt amet consectetur adipisicing elit. Impedit, asci?
-            </p>{' '}
-            <Trash size={20} />{' '}
-          </li>
+          {tasks.map((task) => {
+            return (
+              <li>
+                <CheckCircle size={20} weight='fill' color='var(--purple)' />
+                <p key={task.id}>{task.title}</p> <Trash size={20} />{' '}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
--- src/components/TasksForm.jsx
+++ src/components/TasksForm.jsx
@@ -1,13 +1,15 @@
 import { PlusCircle } from 'phosphor-react';
+import { useState } from 'react';
 import styles from './TasksForm.module.css';
 
 export function TasksForm() {
+  const [newTask, setNewTask] = useState({});
+  const [newTaskList, setNewTaskList] = useState([]);
+
   return (
     <>
       <div className={styles.tasksFormContainer}>
         <form action='' className={styles.taskForm}>
-          {/* <p>hola</p>
-          <p>mundo</p> */}
           <input
             type='text'
             placeholder='Ingresa una nueva tarea'
